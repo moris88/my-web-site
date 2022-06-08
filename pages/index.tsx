1a25eec790e2ef19a249c4e9b05eb37ae953ab29
Maurizio Tolomeo (Moris) <37340833+moris88@users.noreply.github.com>
2022-06-08 09:49:06 +0200

    add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <h1 className="text-3xl font-bold underline bg-red-700">
+          Hello world!
+        </h1>
         <p>main</p>
       </main>
 
