72d3484b9a5ce5dac10d56ea6e2e40e4771034c1
Maurizio Tolomeo <37340833+moris88@users.noreply.github.com>
2024-04-18 19:55:42 +0200

    feat: aggiornamento package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,16 @@
 import type { Config } from 'tailwindcss'
+import flowbite from 'flowbite-react/tailwind'
 
 const config: Config = {
   content: [
-    './node_modules/flowbite-react/lib/**/*.js',
+    flowbite.content(),
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {},
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    flowbite.plugin(),
+  ]
 }
 export default config
