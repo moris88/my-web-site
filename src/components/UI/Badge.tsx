848e2c84c26189ef91119cd84cc9def6bbe91ff5
Maurizio Tolomeo <maurizio.tolomeo@outlook.it>
2025-01-15 18:12:58 +0100

    Update Badge.tsx

--- src/components/UI/Badge.tsx
+++ src/components/UI/Badge.tsx
@@ -14,8 +14,8 @@ function Badge({ children, color, className }: BadgeProps) {
     orange: 'bg-orange-100 text-orange-800 border border-orange-800 dark:bg-orange-600',
     yellow: 'bg-yellow-100 text-yellow-800 border border-yellow-800 dark:bg-yellow-600'
   }
-  const safeClassName = typeof className === 'string' ? className : '';
-  const colorClass = mapColor[color] || 'bg-white text-gray-600 border border-gray-600 dark:bg-gray-200';
+  const safeClassName = typeof className === 'string' ? className : ''
+  const colorClass = mapColor[color] || 'bg-white text-gray-600 border border-gray-600 dark:bg-gray-200'
   return <span className={twMerge(colorClass, 'px-2 py-1 rounded-full inline', safeClassName)}>{children}</span>
 }
 
