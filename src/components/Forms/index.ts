3dc39c3100befa2022729e394b5e42590ab717ae
Maurizio Tolomeo <maurizio.tolomeo@outlook.it>
2025-05-29 21:44:40 +0200

    fix: errori di build

--- src/components/Forms/index.ts
+++ src/components/Forms/index.ts
@@ -1,4 +1,1 @@
 export { default as FormContact } from './FormContact'
-export { default as FormLogin } from './FormLogin'
-export { default as FormArticles } from './FormArticles'
-export { default as FormUser } from './FormUser'
