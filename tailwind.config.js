b73ce58b2713bc3b3fc5e7c3110e434a4d5b138b
Maurizio Tolomeo <maurizio.tolomeo@outlook.it>
2025-11-15 11:29:37 +0100

    chore: aggiorna le dipendenze e modifica la configurazione di Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import { heroui } from '@heroui/react'
 
-export default {
+const config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -23,3 +23,5 @@ export default {
   darkMode: 'class',
   plugins: [heroui()],
 }
+
+export default config
