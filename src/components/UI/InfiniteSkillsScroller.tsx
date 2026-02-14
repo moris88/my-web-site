fc6eb6af66a67305915155005facfbb198649a29
Maurizio Tolomeo <37340833+moris88@users.noreply.github.com>
2026-02-14 11:20:30 +0100

    fix: add console log for rendering skills in InfiniteSkillsScroller

--- src/components/UI/InfiniteSkillsScroller.tsx
+++ src/components/UI/InfiniteSkillsScroller.tsx
@@ -10,6 +10,7 @@ interface InfiniteSkillsScrollerProps {
 function InfiniteSkillsScroller({
 	skills,
 }: Readonly<InfiniteSkillsScrollerProps>) {
+	console.log('Rendering InfiniteSkillsScroller with skills:', skills)
 	return (
 		// Aggiunto h-24 per dare altezza, e fixate le unità arbitrarie [width]
 		<div className="relative m-auto h-36 w-full overflow-hidden bg-gray-50 before:absolute before:top-0 before:left-0 before:z-10 before:h-full before:w-25 before:bg-[linear-gradient(to_right,#f9fafb_0%,rgba(249,250,251,0)_100%)] before:content-[''] after:absolute after:top-0 after:right-0 after:z-10 after:h-full after:w-25 after:bg-[linear-gradient(to_left,#f9fafb_0%,rgba(249,250,251,0)_100%)] after:content-[''] dark:bg-[#1b1a19] dark:after:bg-[linear-gradient(to_left,#1b1a19_0%,rgba(27,26,25,0)_100%)] dark:before:bg-[linear-gradient(to_right,#1b1a19_0%,rgba(27,26,25,0)_100%)]">
